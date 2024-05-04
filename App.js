cd9ab119f8e263aff3789d05bfdfed4b4a8f9a74
mfrandes <mircea.frandes@gloobus.it>
2024-05-04 11:14:02 +0300

    basic layout

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ export default function App() {
         <Button title='Add Goal' />
       </View>
       {/* list of goals */}
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of goals... </Text>
       </View>
     </View>
@@ -18,17 +18,27 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16
   },
   inputContainer: {
+    flex: 1 ,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc'
   },
   textInput: {
     borderWidth: 1,
     borderColor: '#cccccc',
-    width: "80%",
+    width: "70%",
     marginRight: 8,
     padding: 8
+  },
+  goalsContainer: {
+    flex: 4
   }
 });
